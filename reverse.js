4072ac446aac8831b3c4abb9e9ac11b2fa0fe8c0
Suliman Fayyaz <hydrajin@gmail.com>
2021-11-24 14:18:53 -0500

    Removed unecessary code

---
+++ reverse.js
@@ -0,0 +1,12 @@
+const input = process.argv.slice(2); // Remove the extra printed stuff on terminal
+
+const reverse = function(string) {
+  let output = " ";
+  for (let i = string.length - 1; i >= 0; i--) {
+    output += string[i];
+  }
+  return output;
+};
+
+console.log(reverse(`${input} `)); // Takes the input from terminal and then passes it through the reverse function
+
