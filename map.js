63856e7ce63c71d83d924fb6117e9c514c088216
Suliman Fayyaz <hydrajin@gmail.com>
2021-11-26 02:30:38 -0500

    Added assertArraysEqual + EqArrays functions with check cases

--- map.js
+++ map.js
@@ -1,1 +1,40 @@
+// TEST/ASSERTION FUNCTIONS
+const eqArrays = (arr1, arr2) => {
+  let pass = true;
+  if (arr1.length !== arr2.length) {
+    pass = false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      pass = false;
+    }
+  }
+  return pass;
+};
+
+// ASSERT ARRAY EQUAL
+const assertArraysEqual = (arr1, arr2) => {
+  console.log(eqArrays(arr1, arr2) === true ? `✅🟢✅ Assertion Passed: [${arr1}] === [${arr2}]` : `🛑🔴🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+ };
+
 const words = ["my", "name", "is", "Suliman", "Fayyaz"];
+
+
+// DEFINE MAP IN MIN WAY
+// Has to be before being called??
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    // console.log('item BEFORE: ', item);
+    // console.log('item AFTER: ', callback(item));
+    // console.log('---');
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+console.log(results1); // [ 'm', 'n', 'i', 'S', 'F' ]
+// ONLY zero index of words array
+
+console.log(assertArraysEqual(results1, [ 'm', 'n', 'i', 'S', 'F' ])); // [ 'm', 'n', 'i', 'S', 'F' ]
