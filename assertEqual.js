6b86f776e8f0fbbe25f95e945801c38a08736ec2
Suliman Fayyaz <hydrajin@gmail.com>
2021-11-30 03:15:36 -0500

    Removed test conditions from files + added export and require refactor code

--- assertEqual.js
+++ assertEqual.js
@@ -7,10 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("1", 1);
-assertEqual("Hydrajin", "Hydrajin");
-assertEqual("Hydrajin", "hydrajin");
-assertEqual(27, 98);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,11 +1,5 @@
 // assertEqual.js function
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅🟢✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🔴🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 // Create a function head which returns the first item in the array.
 // The head function should not return the first element as an array. It should simply return the element itself
@@ -14,8 +8,4 @@ const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5); // pass
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // pass
-assertEqual(head(["one"]), "one"); // pass
-assertEqual(head([]),); // undefined
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs"); // fail
+module.exports = head;
