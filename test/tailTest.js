f4e94b9b0124ca96989ea430ef4e49491197ca8c
Suliman Fayyaz <hydrajin@gmail.com>
2021-11-30 16:10:56 -0500

    refactored test for mocha/chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,18 +1,27 @@
 
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-const words0 = [];
-tail(words0);
-assertEqual(words0.length, 0);
-const words1 = ["Yo"];
-tail(words1);
-assertEqual(words1.length, 1);
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it(`returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it(`returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it(`returns 2 when words = ["Yo Yo", "Lighthouse", "Labs"]  ;`, () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.equal((tail(words).length), 2); // tails only accepts an array (put .length outside)
+  }); // My original ((tail(words.length)) was wrong since tail only accepts an array NOT a METHOD
+  it(`returns [] when words0 = [] is an empty array by checking array.length === 0 `, () => {
+    const words0 = [];
+    assert.equal((tail(words0).length), 0); // tails only accepts an array (put .length outside)
+  });
+  it(`returns [] when words1 = ["Yo"] or a single element by checking array.length === 0 `, () => {
+    const words1 = ["Yo"];
+    assert.equal((tail(words1).length), 0); // should yeild an empty array for its tail
+  });
+});
