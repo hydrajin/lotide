e38ca8260ca5bff69330f18d89eba69a5039cff9
Suliman Fayyaz <hydrajin@gmail.com>
2021-11-30 14:10:29 -0500

    Added test cases, did I not commit/push earlier?

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,6 @@
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
-
 eqArrays([1, 2, 3], [1, 2, 3]); // => true
 eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
